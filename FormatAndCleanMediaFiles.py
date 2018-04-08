f8860636982846d1749657b41b84063720dbc35a
chris-gomes <chris@ccgomes.com>
2018-04-08 01:30:35 -0400

    finish dummy and derived features

--- FormatAndCleanMediaFiles.py
+++ FormatAndCleanMediaFiles.py
@@ -1,19 +1,21 @@
 
-# %% Import Libraries
+# Import Libraries
 import dask.dataframe as dd
 import pandas as pd
 
-# %% Import csv files
+# Import csv files and create set of company names
 media = dd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\media0*.csv')
-companyNames = pd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\companyNamesFormatted.csv')
+companies = pd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\companyNamesFormatted.csv')
+companyNames = set(companies['Name']) # convert company names to a set
 
-# %% Dummy Codes and Derived Features
+# Dummy Codes and Derived Features
 media['MentionsPerSource'] = media.NumMentions / media.NumSources # derived attribute of mentions per source
-media['InUS'] = media.CountryCode.apply(lambda x: 1 if x == 'US' else 0) # dummy code for if occured in US
-media['NASDAQActor1'] = media.Actor1Name.apply(lambda x: 1 if companyNames['Name'].str.contains(str(x)).any() else 0) # dummy code if actor 1 in NASDAQ
-media['NASDAQActor2'] = media.Actor2Name.apply(lambda x: 1 if companyNames['Name'].str.contains(str(x)).any() else 0) # dummy code if actor 2 in NASDAQ
+media['InUS'] = media.CountryCode.map(lambda x: 1 if x == 'US' else 0) # dummy code for if occured in US
+media['NASDAQActor1'] = media.Actor1Name.map(lambda x: 1 if x in companyNames else 0) # dummy code if actor 1 in NASDAQ
+media['NASDAQActor2'] = media.Actor2Name.map(lambda x: 1 if x in companyNames else 0) # dummy code if actor 2 in NASDAQ
 
-media.head(5)
+# Min-Max Normalization
+
 
 # Export all media to new csv
 media.to_csv(path_or_buf='C:\\Users\ChrisGomes\Projects\MediaStock\media.csv')
