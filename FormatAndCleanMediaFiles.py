02a436d02623b45931f05663145be1f2bce2d889
chris-gomes <chris@ccgomes.com>
2018-04-10 00:26:19 -0400

    project flow added and media formatting script complete except for NA check

--- FormatAndCleanMediaFiles.py
+++ FormatAndCleanMediaFiles.py
@@ -1,21 +1,31 @@
 
 # Import Libraries
-import dask.dataframe as dd
 import pandas as pd
+import numpy as np
 
 # Import csv files and create set of company names
-media = dd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\media0*.csv')
-companies = pd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\companyNamesFormatted.csv')
-companyNames = set(companies['Name']) # convert company names to a set
+defaultPath = 'C:\\Users\ChrisGomes\Projects\MediaStock\media0000000000{0:02d}.csv' # base path for all media csvs
+media = pd.read_csv(defaultPath.format(0)) # add first media csv
+
+for i in range(1, 16):
+    media = media.append(pd.read_csv(defaultPath.format(i)), ignore_index=True) # append each csv numbered after 0
+
+companies = pd.read_csv('C:\\Users\ChrisGomes\Projects\MediaStock\companyNamesFormatted.csv') # Source: https://www.nasdaq.com/screening/company-list.aspx
+companyNames = set(companies.Name) # convert company names to a set
 
 # Dummy Codes and Derived Features
 media['MentionsPerSource'] = media.NumMentions / media.NumSources # derived attribute of mentions per source
-media['InUS'] = media.CountryCode.map(lambda x: 1 if x == 'US' else 0) # dummy code for if occured in US
-media['NASDAQActor1'] = media.Actor1Name.map(lambda x: 1 if x in companyNames else 0) # dummy code if actor 1 in NASDAQ
-media['NASDAQActor2'] = media.Actor2Name.map(lambda x: 1 if x in companyNames else 0) # dummy code if actor 2 in NASDAQ
+media['InUS'] = np.where(media.CountryCode=='US', 1, 0) # dummy code for if occured in US
+media['NASDAQActor1'] = np.where(media.Actor1Name in companyNames, 1, 0) # dummy code if actor 1 in NASDAQ
+media['NASDAQActor2'] = np.where(media.Actor2Name in companyNames, 1, 0) # dummy code if actor 2 in NASDAQ
 
-# Min-Max Normalization
+# check for NAs
 
+# Aggreate media data frame to average out GoldsteinScale, AvgTone, and MentionsPerSource and
+# add up occurances InUS, NASDAQActor1 and NASDAQActor2
+goldToneMSGrouped = media.groupby('MonthYear')['GoldsteinScale', 'AvgTone', 'MentionsPerSource'].mean() # aggregate GoldsteinScale, AvgTone, and MentionsPerSource
+inUSNasdaqGrouped = media.groupby('MonthYear')['InUS', 'NASDAQActor1', 'NASDAQActor2'].sum() # aggregate InUS, NASDAQActor1 and NASDAQActor2
+mediaGrouped = pd.merge(left = goldToneMSGrouped, right = inUSNasdaqGrouped, left_on='MonthYear', 'MonthYear') # merge columns back together
 
 # Export all media to new csv
 media.to_csv(path_or_buf='C:\\Users\ChrisGomes\Projects\MediaStock\media.csv')
