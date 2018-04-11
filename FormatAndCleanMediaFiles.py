9b1c321f263e28be43f0003da0d52a3ebf129492
chris-gomes <chris@ccgomes.com>
2018-04-11 11:49:29 -0400

    bug fixes in script to format media

--- FormatAndCleanMediaFiles.py
+++ FormatAndCleanMediaFiles.py
@@ -1,45 +1,46 @@
+
 # Import Libraries
 import pandas as pd
 import numpy as np
 
-# Import csv files and create set of company names
-defaultPath = '.\data\media0000000000{0:02d}.csv' # base path for all media csvs
-media = pd.read_csv(defaultPath.format(0),
-    dtype={'MonthYear': np.int64,
-    'GoldsteinScale': np.float64,
-    'NumMentions': np.float64,
-    'NumSources': np.float64,
-    'AvgTone': np.float64}) # add first media csv
 
-for i in range(1, 16):
-    media = media.append(pd.read_csv(defaultPath.format(i),
-                                    dtype={'MonthYear': np.int64,
-                                    'GoldsteinScale': np.float64,
-                                    'NumMentions': np.float64,
-                                    'NumSources': np.float64,
-                                    'AvgTone': np.float64}),
-                        ignore_index=True) # append each csv numbered after 0
+def main():
+	#Import csv files and create set of company names
+	media = pd.read_csv('./data/media.csv',
+			    dtype={'Actor1Name':str, 'Actor2Name':str, 'GoldsteinScale':np.float64, 'NumMentions':np.float64, 'NumMentions':np.float64, 'NumSources':np.float64, 'AvgTone':np.float64, 'CountryCode':str}) # Source: GDELT on Google Cloud Platform
+	companies = pd.read_csv('./data/companyNamesFormatted.csv') # Source: https://www.nasdaq.com/screening/company-list.aspx
+	companyNames = set(companies.Name) # convert company names to a set
+	print('Import done') # status output
 
-companies = pd.read_csv('.\data\companyNamesFormatted.csv') # Source: https://www.nasdaq.com/screening/company-list.aspx
-companyNames = set(companies.Name) # convert company names to a set
+	# Dummy Codes and Derived Features (if any of the data is null it is recorded as not in the US or NASDAQ)
+	media['MentionsPerSource'] = media.NumMentions / media.NumSources # derived attribute of mentions per source
+	media['InUS'] = np.where(media.CountryCode=='US', 1, 0) # dummy code for if occured in US
+	media['NASDAQActor1'] = np.where(str(media.Actor1Name) in companyNames, 1, 0) # dummy code if actor 1 in NASDAQ
+	media['NASDAQActor2'] = np.where(str(media.Actor2Name) in companyNames, 1, 0) # dummy code if actor 2 in NASDAQ
+	print('Codes done') # status output
 
-# Dummy Codes and Derived Features
-media['MentionsPerSource'] = media.NumMentions / media.NumSources # derived attribute of mentions per source
-media['InUS'] = np.where(media.CountryCode=='US', 1, 0) # dummy code for if occured in US
-media['NASDAQActor1'] = np.where(media.Actor1Name in companyNames, 1, 0) # dummy code if actor 1 in NASDAQ
-media['NASDAQActor2'] = np.where(media.Actor2Name in companyNames, 1, 0) # dummy code if actor 2 in NASDAQ
+	# remove rows with NAs (decided since the data set has a significant number of rows and imputation would not provide much benefit)
+	media = media.dropna(axis = 0, how = 'any')
+	columns = ['MonthYear', 'GoldsteinScale', 'AvgTone', 'MentionsPerSource', 'InUS', 'NASDAQActor1', 'NASDAQActor2']
+	for name in columns: # check if rows with NAs were actually removed
+		if media[name].isna().any():
+			print('NAs in ' + name)
+	print('NA removal done') # status output
 
-# check if NAs are present in any of the attribute columns
-attributeCols = ['GoldsteinScale', 'AvgTone', 'MentionsPerSource', 'InUS', 'NASDAQActor1', 'NASDAQActor2']
-for name in attributeCols:
-    if pandas.isnull(media[name]).any():
-        print('NAs present in ' + name)
+	# convert MonthYear to int
+	media['MonthYear'] = media.MonthYear.astype('int64')
+	print('MonthYear conversion done')
 
-# Aggreate media data frame to average out GoldsteinScale, AvgTone, and MentionsPerSource and
-# add up occurances InUS, NASDAQActor1 and NASDAQActor2
-goldToneMSGrouped = media.groupby('MonthYear')['GoldsteinScale', 'AvgTone', 'MentionsPerSource'].mean() # aggregate GoldsteinScale, AvgTone, and MentionsPerSource
-inUSNasdaqGrouped = media.groupby('MonthYear')['InUS', 'NASDAQActor1', 'NASDAQActor2'].sum() # aggregate InUS, NASDAQActor1 and NASDAQActor2
-mediaGrouped = pd.merge(left = goldToneMSGrouped, right = inUSNasdaqGrouped, left_on='MonthYear', right_on='MonthYear') # merge columns back together
+	# Aggreate media data frame to average out GoldsteinScale, AvgTone, and MentionsPerSource and
+	# add up occurances InUS, NASDAQActor1 and NASDAQActor2
+	media = media.groupby('MonthYear', as_index=False).agg(
+		{'GoldsteinScale':'mean','AvgTone':'mean','MentionsPerSource':'mean',
+		'InUS':'sum', 'NASDAQActor1':'sum', 'NASDAQActor2':'sum'})
+	print(media.dtypes)
+	print('Agg done') # status check
 
-# Export all media to new csv
-mediaGrouped.to_csv(path_or_buf='.\data\media.csv')
+	# Export all media to new csv
+	media.to_csv(path_or_buf='./data/mediaFormatted.csv')
+
+if __name__ == '__main__':
+	main()
